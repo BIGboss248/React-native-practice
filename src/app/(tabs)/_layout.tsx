ad27f4835d4e9420f7901920e254243b82c82b23
Amin Jamali <53099680+BIGboss248@users.noreply.github.com>
2026-03-28 23:24:00 +0330

    package update and fix

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,8 +3,9 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "crismon" }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "red" }}>
       <Tabs.Screen name="index" options={{ tabBarIcon: ({ color, size, focused }) => <Ionicons name={focused ? "home" : "home-outline"} color={color} size={size} /> }} />
+      <Tabs.Screen name="about" options={{ tabBarIcon: ({ color, size, focused }) => <Ionicons name={focused ? "information-circle" : "information-circle-outline"} color={color} size={size} /> }} />
     </Tabs>
   )
 }
