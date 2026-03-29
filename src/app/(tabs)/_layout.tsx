f84314441c1b3bc86e887e3d078b3cf460af27bb
Amin Jamali <aminjamali79@gmail.com>
2026-03-29 13:51:41 +0330

    made icons separate components

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,11 +1,25 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
+interface IconProps {
+  color: string;
+  size: number;
+  focused: boolean;
+}
+export const InfoIcon = ({ color, size, focused }: IconProps) => <Ionicons name={focused ? "home" : "home-outline"} color={color} size={size} />
+export const aboutIcon = ({ color, size, focused }: IconProps) => <Ionicons name={focused ? "information-circle" : "information-circle-outline"} color={color} size={size} />
+
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "red" }}>
-      <Tabs.Screen name="index" options={{ tabBarIcon: ({ color, size, focused }) => <Ionicons name={focused ? "home" : "home-outline"} color={color} size={size} /> }} />
-      <Tabs.Screen name="about" options={{ tabBarIcon: ({ color, size, focused }) => <Ionicons name={focused ? "information-circle" : "information-circle-outline"} color={color} size={size} /> }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: "red", headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{ tabBarIcon: InfoIcon }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{ tabBarIcon: aboutIcon }}
+      />
     </Tabs>
   )
 }
